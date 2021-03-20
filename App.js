4a3f7cff672c0cc194217d67a8434ced7acf8cec
anoopaneesh <anoopaneesh@gmail.com>
2021-03-20 12:25:25 +0530

    initial

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import BottomTab from "./Navigation/BottomTab";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomTab />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Navigation/BottomTab.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { Entypo , AntDesign } from '@expo/vector-icons'; 
+
+
+const Tab = createBottomTabNavigator()
+
+
+const Home = () => {
+    return <View style={{backgroundColor:'black',flex:1}}><Text style={{backgroundColor:'white'}}>Home</Text></View>
+}
+const Search = () => {
+    return <View style={{backgroundColor:'black',flex:1}}><Text style={{backgroundColor:'white'}}>Search</Text></View>
+}
+
+const options = {
+    showLabel: true,
+    activeTintColor:'white',
+    inactiveTintColor:'gray',
+    style:{
+        borderTopWidth:0,
+        backgroundColor:'black'
+    },
+    labelStyle:{
+        marginBottom:5,
+    }
+
+}
+
+const getIcon = (focused,color,name) => {
+    if(name === 'Home'){
+        return (
+            <View>
+                <Entypo name="home" size={24} color={color} />
+            </View>
+        )
+    }else if(name === 'Search'){
+        return(
+            <View>
+                <AntDesign name="search1" size={24} color={color} />
+            </View>
+            
+        )
+    }
+}
+
+
+
+const BottomTab = () => {
+    return (
+        <Tab.Navigator tabBarOptions={options}>
+            <Tab.Screen name='Home' component={Home} options={{tabBarIcon:({focused,color,size}) => getIcon(focused,color,"Home")}} />
+            <Tab.Screen name='Search' component={Search} options={{tabBarIcon:({focused,color,size}) => getIcon(focused,color,"Search")}} />
+        </Tab.Navigator>
+    )
+}
+
+export default BottomTab
